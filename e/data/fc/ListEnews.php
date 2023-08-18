70d8291732f80034cf2869dcb823b0fc2a369c02
Roger Kam <gandibin@qq.com>
2023-08-19 00:23:51 +0800

    first commit

---
+++ e/data/fc/ListEnews.php
@@ -0,0 +1,6 @@
+<?php
+if(!defined('InEmpireCMS'))
+{
+	exit();
+}
+?><table border=0 cellspacing=0 cellpadding=0><tr><td id="pr1" class="menu1" onclick="chengstate('1')"><a onmouseout=chft(this,0,1) onmouseover=chft(this,1,1) oncontextmenu=ShRM(this,0,1,'',0)>product</a></td></tr><tr id="item1" style="display:none"><td class="list"><table border=0 cellspacing=0 cellpadding=0><tr><td id="pr2" class="file" onclick=""><a onclick=tourl(1,2) onmouseout=chft(this,0,2) onmouseover=chft(this,1,2) oncontextmenu=ShRM(this,1,2,'',1)>product1</a></td></tr><tr><td id="pr4" class="file1" onclick=""><a onclick=tourl(1,4) onmouseout=chft(this,0,4) onmouseover=chft(this,1,4) oncontextmenu=ShRM(this,1,4,'',1)>product2</a></td></tr></table></td></tr><tr><td id="pr5" class="menu3" onclick="chengstate('5')"><a onmouseout=chft(this,0,5) onmouseover=chft(this,1,5) oncontextmenu=ShRM(this,0,5,'',0)>Blog</a></td></tr><tr id="item5" style="display:none"><td class="list1"><table border=0 cellspacing=0 cellpadding=0><tr><td id="pr6" class="file" onclick=""><a onclick=tourl(5,6) onmouseout=chft(this,0,6) onmouseover=chft(this,1,6) oncontextmenu=ShRM(this,5,6,'',1)>blog1</a></td></tr><tr><td id="pr7" class="file1" onclick=""><a onclick=tourl(5,7) onmouseout=chft(this,0,7) onmouseover=chft(this,1,7) oncontextmenu=ShRM(this,5,7,'',1)>blog2</a></td></tr></table></td></tr></table>
---
+++ e/data/fc/searchclass.js
@@ -0,0 +1,1 @@
+document.write("<option value=\'1\'>|-product</option><option value=\'2\'>&nbsp;&nbsp;|-product1</option><option value=\'4\'>&nbsp;&nbsp;|-product2</option><option value=\'5\'>|-Blog</option><option value=\'6\'>&nbsp;&nbsp;|-blog1</option><option value=\'7\'>&nbsp;&nbsp;|-blog2</option>");
